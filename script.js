96546c64e5663f22afde9056167134e3c39fdaff
Sierra MacDonald <sierrainertia@gmail.com>
2020-08-28 00:47:40 -0400

    style changes

--- script.js
+++ script.js
@@ -3,13 +3,11 @@ const projectOneVideo = document.querySelector(".projectOneVideo");
 
 projectOneVideo.addEventListener("mouseover", function () {
   if (!projectOneVideo.playing) {
-    console.log("mouse on");
     projectOneVideo.play();
   }
 });
 
 projectOneVideo.addEventListener("mouseleave", function () {
-  console.log("mouse off");
   projectOneVideo.pause();
 });
 
@@ -18,13 +16,11 @@ const projectTwoVideo = document.querySelector(".projectTwoVideo");
 
 projectTwoVideo.addEventListener("mouseover", function () {
   if (!projectTwoVideo.playing) {
-    console.log("mouse on");
     projectTwoVideo.play();
   }
 });
 
 projectTwoVideo.addEventListener("mouseleave", function () {
-  console.log("mouse off");
   projectTwoVideo.pause();
 });
 
@@ -33,12 +29,10 @@ const projectThreeVideo = document.querySelector(".projectThreeVideo");
 
 projectThreeVideo.addEventListener("mouseover", function () {
   if (!projectThreeVideo.playing) {
-    console.log("mouse on");
     projectThreeVideo.play();
   }
 });
 
 projectThreeVideo.addEventListener("mouseleave", function () {
-  console.log("mouse off");
   projectThreeVideo.pause();
 });
